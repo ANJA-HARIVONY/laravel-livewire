8a2a39394f38fc526c12f33895879a84646c2723
ANJA-HARIVONY <anjaharivoni@gmail.com>
2021-06-25 05:41:09 +0100

    integration bootstrap et premier composent

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <h1>Hello World!</h1>
+    <div style="text-align: center">
+        <button class="btn btn-danger" wire:click="increment">Incremete</button>
+        <h1>{{ $count }}</h1>
+    </div>
+</div>
